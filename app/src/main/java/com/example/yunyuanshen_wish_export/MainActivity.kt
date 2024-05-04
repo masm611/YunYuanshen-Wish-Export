5442c4ea7999470fbb799a093a466ed878dfd696
masm611 <59108894+masm611@users.noreply.github.com>
2024-05-04 18:55:15 +0800

    修正了一些提示信息

--- app/src/main/java/com/example/yunyuanshen_wish_export/MainActivity.kt
+++ app/src/main/java/com/example/yunyuanshen_wish_export/MainActivity.kt
@@ -31,14 +31,12 @@ class YunYuanShenWebviewClient(private val context: MainActivity) : WebViewClien
         view: WebView?,
         request: WebResourceRequest?
     ): WebResourceResponse? {
-        //Log.d("连接截获",request?.url.toString())
         return super.shouldInterceptRequest(view, request)
     }
 
     @Deprecated("Deprecated in Kotlin")
     override fun shouldInterceptRequest(view: WebView?, url: String?): WebResourceResponse? {
-        Log.d("连接截获2！！！", url!!)
-        if (url.startsWith("https://webstatic.mihoyo.com/hk4e/event/e20190909gacha-v3/index.html")) {
+        if (url.startsWith("https://webstatic.mihoyo.com/hk4e/event/e20190909gacha-v3/index.html")) { //判断地址是否是抽卡地址
             Looper.prepare()
             AlertDialog.Builder(context)
                 .setTitle("截获到的地址")
@@ -57,7 +55,7 @@ class YunYuanShenWebviewClient(private val context: MainActivity) : WebViewClien
                 }
                 .setPositiveButton("OK") { dialog, _: Int ->
                     // 用户点击 OK 后的操作
-                    dialog.dismiss() // 可选：关闭对话框
+                    dialog.dismiss() //关闭对话框
                 }
                 .show()
 
@@ -71,16 +69,16 @@ class YunYuanShenWebviewClient(private val context: MainActivity) : WebViewClien
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //保持竖屏
+        //保持竖屏，防止webview重置
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
 
-        //提示用户正确使用方法
+        //提示用户正确使用方法，并叠甲
         AlertDialog.Builder(this)
             .setTitle("本工具使用方法")
-            .setMessage("登录云原神并打开游戏，然后在抽卡祈福界面打开历史记录，程序就会自动推断出抽卡记录分析地址了\n\n开发者:万有引力科技社 · 陈钇林")
+            .setMessage("登录云原神并打开游戏，然后在抽卡祈福界面打开历史记录，程序就会自动推断出抽卡记录分析地址了\n\n注：\n本工具属于第三方工具，但是访问的是官方云原神网站，不采集任何信息\n仅供学习和交流使用，请在24小时内删除\n开发者:masm611@github.com")
             .setPositiveButton("OK") { dialog, _: Int ->
                 // 用户点击 OK 后的操作
-                dialog.dismiss() // 可选：关闭对话框
+                dialog.dismiss() // 关闭对话框
             }
             .show()
 
@@ -89,30 +87,12 @@ class MainActivity : ComponentActivity() {
         setContentView(web)
         web.webViewClient = YunYuanShenWebviewClient(this)
         web.loadUrl("https://ys.mihoyo.com/cloud/m/#/")
-        //web.loadUrl("https://www.wyylkjs.top/")
         //设置基本配置
         val settings = web.settings
-        settings.javaScriptEnabled = true
-        //settings.javaScriptCanOpenWindowsAutomatically = true//js和android交互
+        settings.javaScriptEnabled = true//允许js
+        //settings.javaScriptCanOpenWindowsAutomatically = true//js和android交互，开了的话会有安全问题
         settings.allowFileAccess = true // 允许访问文件
         //settings.setAppCacheEnabled(true) //设置H5的缓存打开,默认关闭
         settings.domStorageEnabled = true//设置可以使用localStorage
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    YunYuanshenWishExportTheme {
-        Greeting("Android")
     }
 }
